2d706cb0d561e927de3486080406fc58150d5543
vosmer <vosmer@nxt.ru>
2016-09-18 22:40:52 +0400

    added categories and list of short news

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,46 @@ module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		react: {
+			all: {
+				files: {
+					'src/js/news.js': [
+						'src/jsx/news.jsx',
+						//'path/to/jsx/templates/dir/input2.jsx'
+					]
+				}
+			}
+		},
+		concat: {
+			options: {
+				separator: "\n",
+				process: function(src, filepath) {
+					return '/*' + filepath + '*/\n' + src;
+				}
+			},
+			js: {
+				options: {
+					separator: ";\n"
+				},
+				files: {
+					'src/js/all.concatenated.js': [
+						'src/js/libs/react.min.js',
+						'src/js/libs/react-dom.min.js',
+						'src/js/news.js'
+					]
+				}
+			}
+		},
 		uglify: {
 			options: {
+				preserveComments: function(node, comment) {
+					return /^@preserve|@license|@cc_on/i.test(comment.value);
+				},
 				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
 			},
-			/*build: {
-				src: 'src/<%= pkg.name %>.js',
-				dest: 'build/<%= pkg.name %>.min.js'
-			}*/
 			js: {
 				files: {
-					'js/all.min.js': ['src/js/react.min.js', 'src/js/libs/react-dom.min.js']
+					'js/all.min.js': ['src/js/all.concatenated.js']
 				}
 			}
 		},
@@ -22,32 +51,41 @@ module.exports = function(grunt) {
 					paths: ['']
 				},
 				files: {
-					'css/result.css': 'src/styles/less/style.less'
+					'css/styles.css': 'src/styles/less/style.less'
 				}
-			},
-			/*production: {
+			}
+		},
+		watch: {
+			dev: {
+				files: ['Gruntfile.js','**.js','src/**'],
+				tasks: ['build'],
 				options: {
-					paths: ['assets/css'],
-					plugins: [
-						new (require('less-plugin-autoprefix'))({browsers: ["last 2 versions"]}),
-						new (require('less-plugin-clean-css'))(cleanCssOptions)
-					],
-					modifyVars: {
-						imgPath: '"http://mycdn.com/path/to/images"',
-						bgColor: 'red'
-					}
-				},
-				files: {
-					'path/to/result.css': 'path/to/source.less'
+					atBegin: true
 				}
-			}*/
+			}
 		}
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-react');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	
 	// Default task(s).
-	grunt.registerTask('default', ['uglify:js','less:development']);
+	grunt.registerTask('default', [
+		'react:all',
+		'concat:js',
+		'uglify:js',
+		'less:development'
+	]);
+	
+	grunt.registerTask('build', [
+		'react:all',
+		'concat:js',
+		//'uglify:js',
+		'less:development'
+	]);
 
 
 };
---
+++ src/js/news.js
@@ -0,0 +1,251 @@
+(function(funcName, baseObj) {
+	// The public function name defaults to window.docReady
+	// but you can pass in your own object and own function name and those will be used
+	// if you want to put them in a different namespace
+	funcName = funcName || "docReady";
+	baseObj = baseObj || window;
+	var readyList = [];
+	var readyFired = false;
+	var readyEventHandlersInstalled = false;
+
+	// call this when the document is ready
+	// this function protects itself against being called more than once
+	function ready() {
+		if (!readyFired) {
+			// this must be set to true before we start calling callbacks
+			readyFired = true;
+			for (var i = 0; i < readyList.length; i++) {
+				// if a callback here happens to add new ready handlers,
+				// the docReady() function will see that it already fired
+				// and will schedule the callback to run right after
+				// this event loop finishes so all handlers will still execute
+				// in order and no new ones will be added to the readyList
+				// while we are processing the list
+				readyList[i].fn.call(window, readyList[i].ctx);
+			}
+			// allow any closures held by these functions to free
+			readyList = [];
+		}
+	}
+
+	function readyStateChange() {
+		if ( document.readyState === "complete" ) {
+			ready();
+		}
+	}
+
+	// This is the one public interface
+	// docReady(fn, context);
+	// the context argument is optional - if present, it will be passed
+	// as an argument to the callback
+	baseObj[funcName] = function(callback, context) {
+		// if ready has already fired, then just schedule the callback
+		// to fire asynchronously, but right away
+		if (readyFired) {
+			setTimeout(function() {callback(context);}, 1);
+			return;
+		} else {
+			// add the function and context to the list
+			readyList.push({fn: callback, ctx: context});
+		}
+		// if document already ready to go, schedule the ready function to run
+		if (document.readyState === "complete") {
+			setTimeout(ready, 1);
+		} else if (!readyEventHandlersInstalled) {
+			// otherwise if we don't have event handlers installed, install them
+			if (document.addEventListener) {
+				// first choice is DOMContentLoaded event
+				document.addEventListener("DOMContentLoaded", ready, false);
+				// backup is window load event
+				window.addEventListener("load", ready, false);
+			} else {
+				// must be IE
+				document.attachEvent("onreadystatechange", readyStateChange);
+				window.attachEvent("onload", ready);
+			}
+			readyEventHandlersInstalled = true;
+		}
+	}
+})("docReady", window);
+
+var vanilla={};
+/*
+ * getJson creates request to server and returns result to passed callback 
+ * @param env 				Object		environment passed to callback on getting succcessfull results
+ * @param requestUrl 		String		url the data to be requested from
+ * @param cb 				Function	callback function fired on successfull request
+ * @param errcb				Function	callback function fired on error (the error text is passed to this calllback)
+ */
+vanilla.getJson=function(env,requestUrl,cb,errcb){
+	
+
+	var request = new XMLHttpRequest();
+	request.open('GET', requestUrl, true);
+
+	request.onload = function() {
+		if (request.status >= 200 && request.status < 400) {
+			// Success!
+			var data = JSON.parse(request.responseText);
+			cb(env,data);
+		} else {
+			errcb("We reached our target server, but it returned an error");
+		}
+	};
+
+	request.onerror = function() {
+		errcb("There was a connection error of some sort");
+	};
+
+	request.send();
+}
+/* news list component START */
+var TheNew = React.createClass({displayName: "TheNew",
+	render: function() {
+		return (
+			React.createElement("li", {className: "theNewItem"}, 
+				React.createElement("span", {className: "newTitle"}, 
+					this.props.title
+				), 
+				React.createElement("span", {className: "newDate"}, 
+					this.props.data.date
+				)
+			)
+		);
+	}
+});
+var NewsList = React.createClass({displayName: "NewsList",
+	render: function() {
+		var newsNodes = this.props.data.map(function(theNew) {
+			return (
+				React.createElement(TheNew, {title: theNew.title, key: theNew.id, data: theNew}, 
+					theNew.title
+				)
+			);
+		});
+		return (
+			React.createElement("ul", {className: "newsList"}, 
+				newsNodes
+			)
+		);
+	}
+});
+//global object for components communication
+var newsBoxGlobalObj = {
+	"category": "" //current category to load
+};
+var NewsBox = React.createClass({displayName: "NewsBox",
+	loadNewsFromServer: function(catId) {
+		if(typeof(catId)=="undefined" || catId===""){return false;}
+		vanilla.getJson(
+			this,
+			'http://testtask.sebbia.com/v1/news/categories/'+catId+'/news',
+			function(env,responseData){
+				env.setState({dataNews: (responseData.list)});
+			},
+			function(errorMessage){
+				console.log("Error getting JSON data from server: ",errorMessage);
+			}
+		);
+	},
+	getInitialState: function() {
+		return {dataNews: []};
+	},
+	componentDidMount: function() {
+		this.loadNewsFromServer("");
+		//setInterval(this.loadCategorysFromServer, this.props.pollInterval); //in case you need to reload it automatically from server
+	},
+	//this is magic react js creators dont want you to know
+	componentWillMount: function() {
+		newsBoxGlobalObj.cb = (data) => {
+			// `this` refers to our react component
+			this.loadNewsFromServer(newsBoxGlobalObj.category);
+		};
+	},
+	render: function() {
+		return (
+			React.createElement("div", {className: "newsBox"}, 
+				React.createElement("h1", null, "Новости"), 
+				React.createElement(NewsList, {data: this.state.dataNews})
+			)
+		);
+	}
+});
+ReactDOM.render(
+	React.createElement(NewsBox, {url: ""}),
+	document.getElementById('news_list')
+);
+/* news list module END */
+
+/* category list module START */
+
+/* category partial component */
+var Category = React.createClass({displayName: "Category",
+	handleCategoryChoose: function(e) {
+		//set category id in global object for newsBox component
+		newsBoxGlobalObj.category=this.props.category.id;
+		//fire callback for newsBox component update
+		newsBoxGlobalObj.cb(e);
+	},
+	render: function() {
+		return (
+			React.createElement("li", {className: "categoryItem", onClick: this.handleCategoryChoose}, 
+				React.createElement("span", {className: "categoryName"}, 
+					this.props.name
+				)
+			)
+		);
+	}
+});
+var CategoryList = React.createClass({displayName: "CategoryList",
+	render: function() {
+		var categoryNodes = this.props.data.map(function(category) {
+			return (
+				React.createElement(Category, {name: category.name, key: category.id, category: category}, 
+					category.name
+				)
+			);
+		});
+		return (
+			React.createElement("ul", {className: "categoriesList"}, 
+				categoryNodes
+			)
+		);
+	}
+});
+
+
+var CategoryBox = React.createClass({displayName: "CategoryBox",
+	loadCategorysFromServer: function() {
+		vanilla.getJson(
+			this,
+			'http://testtask.sebbia.com/v1/news/categories',
+			function(env,d){
+				env.setState({data: (d.list)});
+			},
+			function(errorMessage){
+				console.log("Error getting JSON data from server: ",errorMessage);
+			}
+		);
+	},
+	getInitialState: function() {
+		return {data: []};
+	},
+	componentDidMount: function() {
+		this.loadCategorysFromServer();
+		//setInterval(this.loadCategorysFromServer, this.props.pollInterval);
+	},
+	render: function() {
+		return (
+			React.createElement("div", {className: "categoryBox"}, 
+				React.createElement("h1", null, "Новости по категориям"), 
+				React.createElement(CategoryList, {data: this.state.data})
+			)
+		);
+	}
+});
+
+ReactDOM.render(
+	React.createElement(CategoryBox, {url: "http://testtask.sebbia.com/v1/news/categories", pollInterval: 2000}),
+	document.getElementById('category_list')
+);
+/*category list component END*/
